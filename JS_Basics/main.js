a013e63ac5b7dad3e80f0a920dde15d47bee5744
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-09-25 11:19:45 +0200

    Added first project

---
+++ JS_Basics/main.js
@@ -0,0 +1,35 @@
+// console.log("Hello world");
+
+// alert("Hej hej");
+
+let firstName = "Sebastian"; // string
+
+// console.log("Antal tecken i firstName:", firstName.length);
+// console.log(firstName);
+
+firstName = "Hanna";
+
+// console.log(firstName);
+
+let x = 45;
+
+// console.log(x / 3.14);
+// console.log(x * 3.14);
+// console.log(x - 10);
+
+// console.log(x);
+
+const sum = x + 1;
+
+// console.log("X:", x);
+// console.log("Sum:", sum);
+
+x = x + 1;
+x += 1;
+x++;
+
+// console.log(x);
+
+const y = 11;
+
+console.log(y % 2); // 11/2 = 5 1/2
