c24bdc7ac230bed80d9f4a845695e1b9724155e1
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-10-21 11:43:56 +0200

    Added projects for typescript basics

---
+++ 9. ts-basics/src/main.ts
@@ -0,0 +1,14 @@
+const firstname: string = "Sebastian";
+let age: number = 46;
+const isMarried: boolean = true;
+const lastName: string = "Tegel";
+
+console.log(firstname);
+console.log(age);
+console.log(isMarried);
+
+// age++;
+// age = age + 1;
+// age % 2;
+
+// console.log(firstname + age);
