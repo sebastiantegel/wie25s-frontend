b04eb959a152b8e6836b6fd1bef16ca4f4dc94f0
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-14 09:08:05 +0100

    Added planning

---
+++ Testning/2. TDD/planning.ts
@@ -0,0 +1,17 @@
+// User Story
+// Som en användare vill jag kunna lägga till en uppgift till listan
+
+// Skriva test
+// En lista med uppgifter
+// Ett formulär för inmatning
+// Töm input efter ny todo
+// ul/li för presentation
+
+// 1. Skriva test - Add todo with Spara-button -> 4h
+// 2. Skriva test - Add todo with Enter -> 1h
+
+// 3. Skapa html -> 2h
+
+// 4. Skriv kod -> 6h
+
+// 5. Styling -> 6h
