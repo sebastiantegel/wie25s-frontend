b60e308f20cd0d48f38b2709d6c0cf9a44f49608
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-14 10:10:27 +0100

    Implemented remove click handler

--- Testning/2. TDD/src/htmlUtils.ts
+++ Testning/2. TDD/src/htmlUtils.ts
@@ -7,9 +7,20 @@ export const createHtml = (todos: Todo[]) => {
     ul.innerHTML = "";
   }
 
-  todos.forEach((todo) => {
+  todos.forEach((todo, i) => {
     const li = document.createElement("li");
+    const removeButton = document.createElement("button");
+
     li.innerHTML = todo.text;
+    removeButton.innerHTML = "Ta bort";
+    removeButton.className = "remove";
+
+    removeButton.addEventListener("click", () => {
+      todos.splice(i, 1);
+      createHtml(todos);
+    });
+
+    li.appendChild(removeButton);
 
     ul?.appendChild(li);
   });
