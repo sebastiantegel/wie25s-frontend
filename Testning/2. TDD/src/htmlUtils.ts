01d1e129bb0dd7d0ee52a8badbff23bb1a256fc7
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-14 09:52:41 +0100

    Implemented code to save a new todo

---
+++ Testning/2. TDD/src/htmlUtils.ts
@@ -0,0 +1,16 @@
+import type { Todo } from "./models/Todo";
+
+export const createHtml = (todos: Todo[]) => {
+  const ul = document.getElementById("todos");
+
+  if (ul) {
+    ul.innerHTML = "";
+  }
+
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.innerHTML = todo.text;
+
+    ul?.appendChild(li);
+  });
+};
--- Testning/2. TDD/src/main.ts
+++ Testning/2. TDD/src/main.ts
@@ -1,1 +1,21 @@
+import { createHtml } from "./htmlUtils";
+import { Todo } from "./models/Todo";
 import "./style.css";
+
+const todos: Todo[] = [];
+
+document.getElementById("todoForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const theInput = document.getElementById("todoText");
+
+  let text = "";
+  if (theInput) {
+    text = (theInput as HTMLInputElement).value;
+  }
+
+  const newTodo = new Todo(text);
+  todos.push(newTodo);
+
+  createHtml(todos);
+});
